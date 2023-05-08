daca7efc837dae7c5ab1344b68ddb93308affc92
HaydenMcKenzie <haydendanielmcKenzie@gmail.com>
2023-05-08 11:21:25 +1000

    Beta: Added Skeleton Code and Basic Pseudo

--- ATM_Exam/main/index.py
+++ ATM_Exam/main/index.py
@@ -1,40 +1,9 @@
-'''
-WRITE Please enter your User ID
-INPUT userID
-PRINT Welcome fname lname. Please Enter an Option
-INPUT userOptionChoice
-CASE option of 
-    1: PRINT 1 for Deposits
-    2: PRINT 2 for Withdraw
-    3: PRINT 3 for Balance
-    q: PRINT q to Quit
-OTHERS 
-    PRINT Please enter 1, 2, 3 or q
-ENDCASE
-PRINT Which account do you wish to Withdraw from:
-Input userAccountChoice
-CASE account of 
-    1: PRINT 1 for chequeNo (Cheque)
-    2: PRINT 2 for savingNo (Saving)
-OTHERS 
-    PRINT Please enter 1 or 2
-ENDCASE
-PRINT How much do you wihs to withdraw? Balance = chequeBal or savingBal
-INPUT userWithdrawAmount
-IF amount <= balance THEN
-    Balance - userWithdrawAmount
-    PRINT Withdrawal is successful. Your bank balance is now chequeBal or savingBal
-ELSE:
-    PRINT Error - Wrong Input. Amount Entered (userWithdrawAmount) is greater than amount in account (chequeBal or savingBal) 
-ENDIF
-'''
-
 import csv
 
 
-def StartProgram():
-    global word
-    word = input("Enter a Number: ")
+def startProgram():
+    global userIDInput
+    userIDInput = input("Please enter your User ID: ")
 
 
 def globalOption():
@@ -52,43 +21,30 @@ def dollarMinus(x, y):
 
 def bankingInfo(file_path):
     global accountType
-    global filez
+    global accountFile
     with open(file_path, 'r') as file:
-        filez = [item for item in csv.reader((line.replace("|||", "|") for line in file), delimiter='|')]
-        accountType = [row[0] for row in filez]
+        accountFile = [item for item in csv.reader((line.replace("|||", "|") for line in file), delimiter='|')]
+        accountType = [row[0] for row in accountFile]
 
-StartProgram()
+startProgram()
 
-def search_userID(file_path, word):
+def search_userID(file_path, userIDInput):
     with open(file_path, 'r') as file:
         content = [item for item in csv.reader(file)] #Reads files. Removes ','. Makes in array
         ids = [row[3] for row in content] #Reads verically
         while True:
             try:
-                idIndex = ids.index(word) #001 002 003
+                idIndex = ids.index(userIDInput) #001 002 003
                 people = content[idIndex] #Reads horizonally
                 return people[0:2] #Fname and lname
             except ValueError:
-                word = input("Enter a Number: ")
+                userIDInput = input("Enter a Number: ")
 
 
-name = search_userID("project_ATMExam/main/UserInfo.txt", word) #"project_ATMExam/root/UserInfo.txt"
+name = search_userID("ATM_Exam/main/UserInfo.txt", userIDInput) #"project_ATMExam/root/UserInfo.txt"
 
 def userAccountOption():
     return " ".join(name)
-
-def askingChequeOrSavings():
-    print("Which account do you wish to " + replaceOption + " from:")
-    bankingInfo("project_ATMExam/main/OpeningAccountsData.txt") #"project_ATMExam/root/OpeningAccountsData.txt"
-    if word in accountType:
-        accountTypeIndex = accountType.index(word)
-        peopleAcc = filez[accountTypeIndex]
-        print("1 for " + peopleAcc[1] + " " + "(" + peopleAcc[2] + ")")
-    if word in accountType:
-        accountTypeIndex2 = accountType.index(word) + 1
-        if accountTypeIndex2 < 6:
-            peopleAcc = filez[accountTypeIndex2]
-            print("2 for " + peopleAcc[1] + " " + "(" + peopleAcc[2] + ")")
 
 def askingOption():
     global replaceOption
@@ -112,84 +68,35 @@ def askingOption():
             elif Option == "q":
                 quit()
             else:
-                StartProgram()
-                search_userID("project_ATMExam/main/UserInfo.txt", word)
+                startProgram()
+                search_userID("ATM_Exam/main/UserInfo.txt", userIDInput)
                 userAccountOption()
                 askingOption()
                 pass
         except:
             quit()
 
+def askingChequeOrSavings():
+    print("Which account do you wish to " + replaceOption + " from:")
+    bankingInfo("ATM_Exam/main/OpeningAccountsData.txt") #"project_ATMExam/root/OpeningAccountsData.txt"
+    if userIDInput in accountType:
+        accountTypeIndex = accountType.index(userIDInput)
+        peopleAcc = accountFile[accountTypeIndex]
+        print("1 for " + peopleAcc[1] + " " + "(" + peopleAcc[2] + ")")
+    if userIDInput in accountType:
+        accountTypeIndex2 = accountType.index(userIDInput) + 1
+        if accountTypeIndex2 < 6:
+            peopleAcc = accountFile[accountTypeIndex2]
+            print("2 for " + peopleAcc[1] + " " + "(" + peopleAcc[2] + ")")
+
 askingOption()
 askingChequeOrSavings()
-
-def changing_C_Or_S_Account(file_path):
-    # print("ChandSav = " + ChandSav)
-    # print("Option equals = " + Option)
-    bankingInfo("project_ATMExam/main/OpeningAccountsData.txt")
-    userInput = int(input())
-    test = Option
-
-    if Option == '1':  # Deposits
-        if ChandSav == '1':  # First Option in Bank.txt
-            if word in accountType:
-                accountTypeIndex = accountType.index(word)
-                peopleAcc = filez[accountTypeIndex]
-                y = peopleAcc[3]
-                z = [float(y)]
-                print(dollarAdd(z[0], userInput))
-        elif ChandSav == '2':  # Second Option in Bank.txt
-            if word in accountType:
-                accountTypeIndex2 = accountType.index(word) + 1
-                if accountTypeIndex2 < 6:
-                    peopleAcc = filez[accountTypeIndex2]
-                    y = peopleAcc[3]
-                    z = [float(y)]
-                    print(dollarAdd(z[0], userInput))
-
-    if Option == "2":
-        if ChandSav == "1":
-            if word in accountType:
-                accountTypeIndex = accountType.index(word)
-                peopleAcc = filez[accountTypeIndex]
-                y = peopleAcc[3]
-                z = [float(y)]
-                if z[0] - userInput < 0:
-                    print("Error")
-                    StartProgram()
-                    search_userID("project_ATMExam/main/UserInfo.txt", word)
-                    userAccountOption()
-                    askingOption()
-                    askingChequeOrSavings()
-                    chequeAndSavings("project_ATMExam/main/OpeningAccountsData.txt")
-                    changing_C_Or_S_Account("project_ATMExam/main/OpeningAccountsData.txt")
-                else:
-                    print(dollarMinus(z[0], userInput))
-
-        elif ChandSav == "2":
-            if word in accountType:
-                accountTypeIndex2 = accountType.index(word) + 1
-                if accountTypeIndex2 < 6:
-                    peopleAcc = filez[accountTypeIndex2]
-                    y = peopleAcc[3]
-                    z = [float(y)]
-                    if z[0] - userInput < 0:
-                        print("Error")
-                        StartProgram()
-                        search_userID("project_ATMExam/main/UserInfo.txt", word)
-                        userAccountOption()
-                        askingOption()
-                        askingChequeOrSavings()
-                        chequeAndSavings("project_ATMExam/main/OpeningAccountsData.txt")
-                        changing_C_Or_S_Account("project_ATMExam/main/OpeningAccountsData.txt")
-                    else:
-                        print(dollarMinus(z[0], userInput))
 
 def chequeAndSavings(file_path):
     global ChandSav
     global replaceOption2
-    bankingInfo("project_ATMExam/main/OpeningAccountsData.txt")
-    test = word
+    bankingInfo("ATM_Exam/main/OpeningAccountsData.txt")
+    test = userIDInput
 
     ChandSav = input()
 
@@ -201,15 +108,15 @@ def chequeAndSavings(file_path):
         replaceOption2 = Option.replace("3", "Balance")
         if ChandSav == '1':
             if test in accountType:
-                accountTypeIndex = accountType.index(word)
-                peopleAcc = filez[accountTypeIndex]  # Changes to a int
+                accountTypeIndex = accountType.index(userIDInput)
+                peopleAcc = accountFile[accountTypeIndex]  # Changes to a int
                 print(peopleAcc[3])
                 quit()
         if ChandSav == '2':
             if test in accountType:
-                accountTypeIndex2 = accountType.index(word) + 1
+                accountTypeIndex2 = accountType.index(userIDInput) + 1
                 if accountTypeIndex2 < 6:
-                    peopleAcc = filez[accountTypeIndex2]
+                    peopleAcc = accountFile[accountTypeIndex2]
                     print(peopleAcc[3])
                     quit()
 
@@ -217,47 +124,109 @@ def chequeAndSavings(file_path):
         try:
             if ChandSav == '1':
                 if test in accountType:
-                    accountTypeIndex = accountType.index(word)
-                    peopleAcc = filez[accountTypeIndex]
+                    accountTypeIndex = accountType.index(userIDInput)
+                    peopleAcc = accountFile[accountTypeIndex]
                     print("How much do you wish to " + replaceOption2 + "? Balance = " + peopleAcc[3])
                     break
             elif ChandSav == '2':
                 if test in accountType:
-                    accountTypeIndex2 = accountType.index(word) + 1
+                    accountTypeIndex2 = accountType.index(userIDInput) + 1
                     if accountTypeIndex2 < 6:
-                        peopleAcc = filez[accountTypeIndex2]
+                        peopleAcc = accountFile[accountTypeIndex2]
                         print("How much do you wish to " + replaceOption2 + "? Balance = " + peopleAcc[3])
                         break
             else:
-                StartProgram()
-                search_userID("project_ATMExam/main/UserInfo.txt", word)
+                startProgram()
+                search_userID("ATM_Exam/main/UserInfo.txt", userIDInput)
                 userAccountOption()
                 askingOption()
                 askingChequeOrSavings()
-                chequeAndSavings("project_ATMExam/main/OpeningAccountsData.txt")
+                chequeAndSavings("ATM_Exam/main/OpeningAccountsData.txt")
                 break
         except:
             quit()
 
-chequeAndSavings("project_ATMExam/main/OpeningAccountsData.txt")
-changing_C_Or_S_Account("project_ATMExam/main/OpeningAccountsData.txt")
+def changing_C_Or_S_Account(file_path):
+    # print("ChandSav = " + ChandSav)
+    # print("Option equals = " + Option)
+    bankingInfo("ATM_Exam/main/OpeningAccountsData.txt")
+    userInput = int(input())
+
+    if Option == '1':  # Deposits
+        if ChandSav == '1':  # First Option in Bank.txt
+            if userIDInput in accountType:
+                accountTypeIndex = accountType.index(userIDInput)
+                peopleAcc = accountFile[accountTypeIndex]
+                y = peopleAcc[3]
+                z = [float(y)]
+                print(dollarAdd(z[0], userInput))
+        elif ChandSav == '2':  # Second Option in Bank.txt
+            if userIDInput in accountType:
+                accountTypeIndex2 = accountType.index(userIDInput) + 1
+                if accountTypeIndex2 < 6:
+                    peopleAcc = accountFile[accountTypeIndex2]
+                    y = peopleAcc[3]
+                    z = [float(y)]
+                    print(dollarAdd(z[0], userInput))
+
+    if Option == "2":
+        if ChandSav == "1":
+            if userIDInput in accountType:
+                accountTypeIndex = accountType.index(userIDInput)
+                peopleAcc = accountFile[accountTypeIndex]
+                y = peopleAcc[3]
+                z = [float(y)]
+                if z[0] - userInput < 0:
+                    print("Error")
+                    startProgram()
+                    search_userID("ATM_Exam/main/UserInfo.txt", userIDInput)
+                    userAccountOption()
+                    askingOption()
+                    askingChequeOrSavings()
+                    chequeAndSavings("ATM_Exam/main/OpeningAccountsData.txt")
+                    changing_C_Or_S_Account("ATM_Exam/main/OpeningAccountsData.txt")
+                else:
+                    print(dollarMinus(z[0], userInput))
+
+        elif ChandSav == "2":
+            if userIDInput in accountType:
+                accountTypeIndex2 = accountType.index(userIDInput) + 1
+                if accountTypeIndex2 < 6:
+                    peopleAcc = accountFile[accountTypeIndex2]
+                    y = peopleAcc[3]
+                    z = [float(y)]
+                    if z[0] - userInput < 0:
+                        print("Error")
+                        startProgram()
+                        search_userID("ATM_Exam/main/UserInfo.txt", userIDInput)
+                        userAccountOption()
+                        askingOption()
+                        askingChequeOrSavings()
+                        chequeAndSavings("ATM_Exam/main/OpeningAccountsData.txt")
+                        changing_C_Or_S_Account("ATM_Exam/main/OpeningAccountsData.txt")
+                    else:
+                        print(dollarMinus(z[0], userInput))
+
+chequeAndSavings("ATM_Exam/main/OpeningAccountsData.txt")
+changing_C_Or_S_Account("ATM_Exam/main/OpeningAccountsData.txt")
 
 def startOverAgain():
     print("End")
 
 startOverAgain()
+
 '''                   
         if Option == "2":
             if "003" in accountType:
                 accountTypeIndex = accountType.index(test)
-                peopleAcc = filez[accountTypeIndex]
+                peopleAcc = accountFile[accountTypeIndex]
                 y = peopleAcc[3]  # Selects all numerical numbers
                 z = [float(y)]  # Changes to a int
                 print(z[0] - userInput)
             if test in accountType:
                 accountTypeIndex2 = accountType.index(test) + 1
                 if accountTypeIndex2 < 6:
-                    peopleAcc = filez[accountTypeIndex2]
+                    peopleAcc = accountFile[accountTypeIndex2]
                     y = peopleAcc[3]  # Selects all numerical numbers
                     z = [float(y)]  # Changes to a int
                     print(z[0] - userInput)
